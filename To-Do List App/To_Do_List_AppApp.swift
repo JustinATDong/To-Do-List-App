7725d06b12936d0e16251613a01dc5592faca6e8
JustinATDong <donganhtuanjustin@gmail.com>
2024-04-27 13:30:29 -0700

    Preliminary App setup

---
+++ To-Do List App/To_Do_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_Do_List_AppApp.swift
+//  To-Do List App
+//
+//  Created by Justin Dong on 4/27/24.
+//
+
+import SwiftUI
+
+@main
+struct To_Do_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
